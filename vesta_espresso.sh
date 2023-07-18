5943ccdcc6a9db1cb5446121126ba6871e4ec168
Omar Ashour <omar.ashour22@gmail.com>
2023-07-18 16:43:51 -0700

    Better support for paths with spaces

--- vesta_espresso.sh
+++ vesta_espresso.sh
@@ -10,7 +10,7 @@
 # the same directory as the input, and opens it in VESTA.
 
 if [ -z "$PMG_VENV" ]; then
-	PMG_VENV="$HOME/.vesta_espresso_venv"
+	PMG_VENV="$PWD/venv"
 fi
 if [ -z "$VESTA" ]; then
 	VESTA="/Applications/VESTA/VESTA.app/Contents/MacOS/VESTA"
@@ -22,10 +22,10 @@ for f in "$@"; do
     ext="${f##*.}"
     if [[ $ext == "pwi" || $ext == "in" ]]; then
         # Set up log file in the same directory as the the first input
-        log=$(dirname $f)/.qe-vesta.log
-        rm -f ${log}
+        log="$(dirname "${f}")/.vesta-espresso.log"
+        rm -f "${log}" # Delete left over logs
         # Activate venv
-        source ${PMG_VENV}/bin/activate
+        source "${PMG_VENV}/bin/activate"
         qe_found=true
         break
     fi
@@ -39,25 +39,25 @@ else
     temp_files=()
     perm_files=()
     for f in "$@"; do
-        dir=$(dirname ${f})
-        basename=$(basename ${f})
+        dir=$(dirname "${f}")
+        basename=$(basename "${f}")
         ext="${f##*.}"
         # If the file is .pwi or .in (i.e., QE), make temporary .cif
         if [[ $ext == "pwi" || $ext == "in" ]]; then
             cif=${dir}/.${basename}.cif
             temp_files+=("${cif}")
-            echo -e "Input: ${f}\nOutput: ${cif}\n****" >> $log 2>&1
+            echo -e "Input: ${f}\nOutput: ${cif}\n****" >> "${log}" 2>&1
             python -c "from pymatgen.io.espresso.inputs import PWin;\
                     struct = PWin.from_file('${f}').structure;\
-                    struct.to('${cif}');" >> $log 2>&1
-            echo -e "\n----" >> $log 2>&1
+                    struct.to('${cif}');" >> "${log}" 2>&1
+            echo -e "\n----" >> "${log}" 2>&1
         else
             # Otherwise, just use the file as-is
             perm_files+=("${f}")
         fi
     done
 
-    echo -e "Opening VESTA...\n" >> $log 2>&1
-    echo -e "Command: $VESTA ${temp_files[@]} ${perm_files[@]}\n" >> $log 2>&1
-    $VESTA ${temp_files[@]} ${perm_files[@]} && rm ${temp_files[@]}
+    echo -e "Opening VESTA...\n" >> "${log}" 2>&1
+    echo -e "Command: $VESTA ${temp_files[@]} ${perm_files[@]}\n" >> "${log}" 2>&1
+    $VESTA "${temp_files[@]}" "${perm_files[@]}" && rm "${temp_files[@]}"
 fi
